7546356801596a94cea9958a8a853b1819ad1856
alfathony <alfanoxe@gmail.com>
2024-02-29 14:10:10 +0700

    add second

--- js/script.js
+++ js/script.js
@@ -14,3 +14,24 @@ function setName(nama) {
     document.getElementById('name').innerHTML = nama;
     document.getElementById('error-name').innerHTML = '';
 }
+
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function nextSlide(n) {
+    showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+    let listImage = document.getElementsByClassName('header-img');
+    if (n > listImage.length) slideIndex = 1;
+    let i = 0;
+    while (i < listImage.length) {
+        listImage[i].Style.display = 'none';
+        i++;
+    }
+    listImage[slideIndex - 1].style.display = 'block';
+    console.log(slideIndex);
+}
+
+const slideInterval = setInterval(() => nextSlide(1), 2000);
