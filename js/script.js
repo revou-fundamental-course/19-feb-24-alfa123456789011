fd2a165712ff8c363a8fe4e88b487bf5263be3d8
alfathony <alfanoxe@gmail.com>
2024-02-29 17:11:37 +0700

    change 4

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ function showSlide(n) {
     if (n > listImage.length) slideIndex = 1;
     let i = 0;
     while (i < listImage.length) {
-        listImage[i].Style.display = 'none';
+        listImage[i].style.display = 'none';
         i++;
     }
     listImage[slideIndex - 1].style.display = 'block';
@@ -35,4 +35,22 @@ function showSlide(n) {
 }
 
 const slideInterval = setInterval(() => nextSlide(1), 2000);
+
+
+function sendForm() {
+    let date = new Date().toLocaleString();
+    let fname = document.forms['message-us-form']['fname'];
+    let lname = document.forms['message-us-form']['lname'];
+    let message = document.forms['message-us-form']['Message'];
+    
+    if (fname.value == '' || lname.value == '') {
+        alert('Mohon cek kembali');
+    } else {
+        document.getElementById('current-time').innerHTML = date;
+        document.getElementById('full-name').innerHTML = fname.value + ' ' + lname.value;
+        document.getElementById('result-message').innerHTML = message.value;
+    }
+    return false;
+}
+
 
